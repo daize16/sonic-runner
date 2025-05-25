eba75d242cc365101b0d246bf04518732936a73f
Dwaraka <dwarkivek@gmail.com>
2025-05-25 22:01:30 +0530

    add sonic game object

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -1,4 +1,5 @@
 import k from "../kaplayCtx";
+import { makeSonic } from "../entities/sonic"
 
 export default function mainMenu() {
   if (!k.getData("best-score")) k.setData("best-score", 0);
@@ -33,12 +34,13 @@ export default function mainMenu() {
   ]);
 
 
-  const gameSpeed = 4000;
+  const gameSpeed = 1000;
+  makeSonic(k.vec2(200, 745));
   k.onUpdate(() => {
     if (bgPieces[1].pos.x < 0) {
       bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
       bgPieces.push(bgPieces.shift());
-    }
+    };
 
     bgPieces[0].move(-100, 0);
     bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
@@ -46,7 +48,7 @@ export default function mainMenu() {
     if (platforms[1].pos.x < 0) {
       platforms[0].moveTo(platforms[1].pos.x + platforms[1].width * 4, 450);
       platforms.push(platforms.shift());
-    }
+    };
 
     platforms[0].move(-gameSpeed, 0);
     platforms[1].moveTo(platforms[0].pos.x + platforms[1].width * 4, 450);
